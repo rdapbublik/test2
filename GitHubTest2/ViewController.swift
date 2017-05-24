ce836ef22cbd9fa8bee7c7bad5743d3a048aef66
Denis Rubtsov <dr@iMac-Denis.local>
2017-05-24 14:30:40 +0300

    Initial Commit

---
+++ GitHubTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubTest2
+//
+//  Created by Denis Rubtsov on 24.05.17.
+//  Copyright © 2017 Denis Rubtsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
